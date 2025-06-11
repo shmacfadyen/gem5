27ad971e1a1e4108eea0377f5f4336541da9f037
shmacfadyen <shmacfadyen@oakland.edu>
2025-06-10 21:38:28 -0400

    Add test compilation files

--- configs/learning_gem5/part1/simple-arm.py
+++ configs/learning_gem5/part1/simple-arm.py
@@ -59,8 +59,7 @@ system.system_port = system.membus.cpu_side_ports
 thispath = os.path.dirname(os.path.realpath(__file__))
 binary = os.path.join(
     thispath,
-    "../../../",
-    "tests/test-progs/hello/bin/arm/linux/hello",
+    "test_files/multihelloworld-arm",
 )
 
 system.workload = SEWorkload.init_compatible(binary)
--- configs/learning_gem5/part1/simple-riscv.py
+++ configs/learning_gem5/part1/simple-riscv.py
@@ -59,8 +59,7 @@ system.system_port = system.membus.cpu_side_ports
 thispath = os.path.dirname(os.path.realpath(__file__))
 binary = os.path.join(
     thispath,
-    "../../../",
-    "tests/test-progs/hello/bin/riscv/linux/hello",
+    "test_files/multihelloworld-riscv",
 )
 
 system.workload = SEWorkload.init_compatible(binary)
