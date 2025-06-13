e38f38d3bddb54d013711296162c0a9499dc52a5
djoshd1 <joshuaduncan@oakland.edu>
2025-06-12 20:54:09 -0400

    Josh Third commit

--- configs/josh_configs/simple-arm.py
+++ configs/josh_configs/simple-arm.py
@@ -32,45 +32,67 @@ X86 ISA). More detailed documentation can be found in `simple.py`.
 import m5
 from m5.objects import *
 
+# create the system we are going to simulate
 system = System()
 
+# Set the clock frequency of the system (and all of its children)
 system.clk_domain = SrcClockDomain()
 system.clk_domain.clock = "1GHz"
 system.clk_domain.voltage_domain = VoltageDomain()
 
-system.mem_mode = "timing"
-system.mem_ranges = [AddrRange("512MiB")]
+# Set up the system
+system.mem_mode = "timing" # Use timing accesses
+system.mem_ranges = [AddrRange("512MiB")] # Create an address range
 system.cpu = ArmTimingSimpleCPU()
 
+# Create a memory bus, a system crossbar, in this case
 system.membus = SystemXBar()
 
+# Hook the CPU ports up to the membus
 system.cpu.icache_port = system.membus.cpu_side_ports
 system.cpu.dcache_port = system.membus.cpu_side_ports
 
+# create the interrupt controller for the CPU and connect to the membus
 system.cpu.createInterruptController()
 
+# Create a DDR3 memory controller and connect it to the membus
 system.mem_ctrl = MemCtrl()
 system.mem_ctrl.dram = DDR3_1600_8x8()
 system.mem_ctrl.dram.range = system.mem_ranges[0]
 system.mem_ctrl.port = system.membus.mem_side_ports
 
+# Connect the system up to the membus
 system.system_port = system.membus.cpu_side_ports
 
+#Branch Prediction
+system.cpu.branchPred = GAgBP()
+
+# Here we set the arm "hello world" binary. With other ISAs you must specify
+# workloads compiled to those ISAs. Other "hello world" binaries for other ISAs
+# can be found in "tests/test-progs/hello".
 thispath = os.path.dirname(os.path.realpath(__file__))
 binary = os.path.join(
     thispath,
-    "../../../",
-    "tests/test-progs/hello/bin/arm/linux/hello",
+    #Sets up three directory levels
+    "../../",
+    #Change to the path of our program
+    "configs/learning_gem5/part1/test_files/multihelloworld-arm",
 )
 
 system.workload = SEWorkload.init_compatible(binary)
 
+# Create a process for a simple "Hello World" application
 process = Process()
+# Set the command
+# cmd is a list which begins with the executable (like argv)
 process.cmd = [binary]
+# Set the cpu to use the process as its workload and create thread contexts
 system.cpu.workload = process
 system.cpu.createThreads()
 
+# set up the root SimObject and start the simulation
 root = Root(full_system=False, system=system)
+# instantiate all of the objects we've created above
 m5.instantiate()
 
 print(f"Beginning simulation!")
